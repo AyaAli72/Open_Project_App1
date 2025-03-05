df8efabc54460c6fb2c88e3527056daa526d6109
Aya Ali <ayaali5202@gmail.com>
2025-03-05 14:56:02 +0200

    change to button

--- just_a_min/lib/main.dart
+++ just_a_min/lib/main.dart
@@ -18,52 +18,115 @@ class MyApp extends StatelessWidget {
                   fontWeight: FontWeight.w800),
               backgroundColor: Colors.blue,
             ),
-            body: Container(
-                alignment: Alignment.bottomCenter,
-                padding: const EdgeInsets.all(20),
-                margin: const EdgeInsets.all(20),
-                decoration: BoxDecoration(
-                    color: const Color.fromARGB(255, 88, 137, 221),
-                    borderRadius: BorderRadius.circular(20)),
-                width: 400,
-                height: 700,
-                child: const Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Text(
+            body: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  const SizedBox(height: 5, width: 5),
+                  ElevatedButton(
+                    onPressed: () {},
+                    child: const Text(
                       "Patient",
                       style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 30,
-                          fontWeight: FontWeight.bold,
-                          textBaseline: TextBaseline.alphabetic),
+                        color: Colors.white,
+                        fontSize: 50,
+                        fontWeight: FontWeight.bold,
+                      ),
                     ),
-                    Text(
-                      "Doctore",
+                  ),
+                  const SizedBox(height: 5, width: 5),
+                  ElevatedButton(
+                    onPressed: () {},
+                    // style:  ElevatedButton.styleFrom(
+                    //   shape:  RoundedRectangleBorder(
+                    //     borderRadius:  BorderRadius.circular(10)
+                    //   )
+                    // ),
+                    child: const Text(
+                      "Doctor",
                       style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 30,
-                          fontWeight: FontWeight.bold,
-                          textBaseline: TextBaseline.alphabetic),
+                        color: Colors.white,
+                        fontSize: 50,
+                        fontWeight: FontWeight.bold,
+                      ),
                     ),
-                    Text(
-                      "Pharmacy",
-                      style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 30,
-                          fontWeight: FontWeight.bold,
-                          textBaseline: TextBaseline.alphabetic),
-                    ),
-                    Text(
+                  ),
+                  const SizedBox(height: 5, width: 5),
+                  ElevatedButton(
+                    onPressed: () {},
+                    child: const Text(
                       "Hospital",
                       style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 30,
-                          fontWeight: FontWeight.bold,
-                          textBaseline: TextBaseline.alphabetic),
-                    )
-                  ],
-                ))));
+                        color: Colors.white,
+                        fontSize: 50,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 5, width: 5),
+                  ElevatedButton(
+                    onPressed: () {},
+                    child: const Text(
+                      "Pharmacy",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 50,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 5, width: 5),
+                ],
+              ),
+            )
+            // body: Container(
+
+            //     alignment: Alignment.bottomCenter,
+            //     padding: const EdgeInsets.all(20),
+            //     margin: const EdgeInsets.all(20),
+            //     decoration: BoxDecoration(
+            //         color: const Color.fromARGB(255, 88, 137, 221),
+            //         borderRadius: BorderRadius.circular(20)),
+            //     width: 400,
+            //     height: 700,
+            //     child: const Column(
+            //       mainAxisAlignment: MainAxisAlignment.spaceAround,
+            //       crossAxisAlignment: CrossAxisAlignment.center,
+            //       children: [
+            //         Text(
+            //           "Patient",
+            //           style: TextStyle(
+            //               color: Colors.white,
+            //               fontSize: 30,
+            //               fontWeight: FontWeight.bold,
+            //               textBaseline: TextBaseline.alphabetic),
+            //         ),
+            //         Text(
+            //           "Doctore",
+            //           style: TextStyle(
+            //               color: Colors.white,
+            //               fontSize: 30,
+            //               fontWeight: FontWeight.bold,
+            //               textBaseline: TextBaseline.alphabetic),
+            //         ),
+            //         Text(
+            //           "Pharmacy",
+            //           style: TextStyle(
+            //               color: Colors.white,
+            //               fontSize: 30,
+            //               fontWeight: FontWeight.bold,
+            //               textBaseline: TextBaseline.alphabetic),
+            //         ),
+            //         Text(
+            //           "Hospital",
+            //           style: TextStyle(
+            //               color: Colors.white,
+            //               fontSize: 30,
+            //               fontWeight: FontWeight.bold,
+            //               textBaseline: TextBaseline.alphabetic),
+            //         )
+            //       ],
+            // ))
+            ));
   }
 }
