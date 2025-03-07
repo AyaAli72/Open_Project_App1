54833372b136466c8b74964cdf764cbc43edceca
Aya Ali <ayaali5202@gmail.com>
2025-03-07 19:49:04 +0200

    adding new page

--- just_a_min/lib/main.dart
+++ just_a_min/lib/main.dart
@@ -8,125 +8,370 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        home: Scaffold(
-            appBar: AppBar(
-              title: const Text("Just a minute."),
-              titleTextStyle: const TextStyle(
-                  color: Colors.white,
-                  fontSize: 35.2,
-                  fontWeight: FontWeight.w800),
-              backgroundColor: Colors.blue,
-            ),
-            body: Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  const SizedBox(height: 5, width: 5),
-                  ElevatedButton(
-                    onPressed: () {},
-                    child: const Text(
-                      "Patient",
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 50,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ),
-                  const SizedBox(height: 5, width: 5),
-                  ElevatedButton(
-                    onPressed: () {},
-                    // style:  ElevatedButton.styleFrom(
-                    //   shape:  RoundedRectangleBorder(
-                    //     borderRadius:  BorderRadius.circular(10)
-                    //   )
-                    // ),
-                    child: const Text(
-                      "Doctor",
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 50,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ),
-                  const SizedBox(height: 5, width: 5),
-                  ElevatedButton(
-                    onPressed: () {},
-                    child: const Text(
-                      "Hospital",
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 50,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ),
-                  const SizedBox(height: 5, width: 5),
-                  ElevatedButton(
-                    onPressed: () {},
-                    child: const Text(
-                      "Pharmacy",
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 50,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ),
-                  const SizedBox(height: 5, width: 5),
-                ],
-              ),
-            )
-            // body: Container(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
 
-            //     alignment: Alignment.bottomCenter,
-            //     padding: const EdgeInsets.all(20),
-            //     margin: const EdgeInsets.all(20),
-            //     decoration: BoxDecoration(
-            //         color: const Color.fromARGB(255, 88, 137, 221),
-            //         borderRadius: BorderRadius.circular(20)),
-            //     width: 400,
-            //     height: 700,
-            //     child: const Column(
-            //       mainAxisAlignment: MainAxisAlignment.spaceAround,
-            //       crossAxisAlignment: CrossAxisAlignment.center,
-            //       children: [
-            //         Text(
-            //           "Patient",
-            //           style: TextStyle(
-            //               color: Colors.white,
-            //               fontSize: 30,
-            //               fontWeight: FontWeight.bold,
-            //               textBaseline: TextBaseline.alphabetic),
-            //         ),
-            //         Text(
-            //           "Doctore",
-            //           style: TextStyle(
-            //               color: Colors.white,
-            //               fontSize: 30,
-            //               fontWeight: FontWeight.bold,
-            //               textBaseline: TextBaseline.alphabetic),
-            //         ),
-            //         Text(
-            //           "Pharmacy",
-            //           style: TextStyle(
-            //               color: Colors.white,
-            //               fontSize: 30,
-            //               fontWeight: FontWeight.bold,
-            //               textBaseline: TextBaseline.alphabetic),
-            //         ),
-            //         Text(
-            //           "Hospital",
-            //           style: TextStyle(
-            //               color: Colors.white,
-            //               fontSize: 30,
-            //               fontWeight: FontWeight.bold,
-            //               textBaseline: TextBaseline.alphabetic),
-            //         )
-            //       ],
-            // ))
-            ));
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          "Just a minute.",
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 35.2,
+            fontWeight: FontWeight.w800,
+          ),
+        ),
+        backgroundColor: Colors.blue,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => patient_page()));
+                // Add navigation or functionality here
+              },
+              style: ElevatedButton.styleFrom(
+                padding: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+                backgroundColor: Colors.blue, // Button background color
+              ),
+              child: const Text(
+                "Patient",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 45,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => Doctor_page()));
+              },
+              style: ElevatedButton.styleFrom(
+                padding: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+                backgroundColor: Colors.blue, // Button background color
+              ),
+              child: const Text(
+                "Doctor",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 45,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => Hospital_page(),
+                  ),
+                );
+              },
+              style: ElevatedButton.styleFrom(
+                padding: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+                backgroundColor: Colors.blue, // Button background color
+              ),
+              child: const Text(
+                "Hospital",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 45,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => Pharmacy_page(),
+                  ),
+                );
+              },
+              style: ElevatedButton.styleFrom(
+                padding: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+                backgroundColor: Colors.blue, // Button background color
+              ),
+              child: const Text(
+                "Pharmacy",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 40,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class patient_page extends StatefulWidget {
+  @override
+  _PatientPageState createState() => _PatientPageState();
+}
+
+class _PatientPageState extends State<patient_page> {
+  final TextEditingController _controller = TextEditingController();
+
+  @override
+  void dispose() {
+    _controller.dispose(); // Dispose the controller when the widget is removed
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Patient Page'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            TextField(
+              controller: _controller,
+              decoration: InputDecoration(
+                labelText: 'Enter your name',
+                border: OutlineInputBorder(),
+              ),
+            ),
+            SizedBox(height: 20),
+            TextField(
+              controller: _controller,
+              decoration: InputDecoration(
+                labelText: 'Enter your email',
+                border: OutlineInputBorder(),
+              ),
+            ),
+            SizedBox(height: 20),
+            TextField(
+              controller: _controller,
+              decoration: InputDecoration(
+                labelText: 'Enter your Phone Number',
+                border: OutlineInputBorder(),
+              ),
+            ),
+            SizedBox(height: 20),
+            TextField(
+               maxLines:18,
+              controller: _controller,
+              decoration: InputDecoration(
+                labelText: 'If you have a illness History Please enter it.',
+                border: OutlineInputBorder(),
+              ),
+            ),
+            SizedBox(height: 20),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class Doctor_page extends StatefulWidget {
+  @override
+  _DoctorPageState createState() => _DoctorPageState();
+}
+
+class _DoctorPageState extends State<Doctor_page> {
+  final TextEditingController _controller = TextEditingController();
+
+  @override
+  void dispose() {
+    _controller.dispose(); // Dispose the controller when the widget is removed
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Doctor Page'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            TextField(
+              controller: _controller,
+              decoration: InputDecoration(
+                labelText: 'Enter your name',
+                border: OutlineInputBorder(),
+              ),
+            ),
+            SizedBox(height: 20),
+            TextField(
+              controller: _controller,
+              decoration: InputDecoration(
+                labelText: 'Enter your email',
+                border: OutlineInputBorder(),
+              ),
+            ),
+            SizedBox(height: 20),
+            TextField(
+              controller: _controller,
+              decoration: InputDecoration(
+                labelText: 'Enter your Phone Number',
+                border: OutlineInputBorder(),
+              ),
+            ),
+            SizedBox(height: 20),
+            TextField(
+              controller: _controller,
+              decoration: InputDecoration(
+                labelText: 'Enter Your Specialization.',
+                border: OutlineInputBorder(),
+              ),
+            ),
+            SizedBox(height: 20),
+            TextField(
+               maxLines:15 ,
+              controller: _controller,
+              decoration: InputDecoration(
+                labelText: 'Enter your certificates.',
+                border: OutlineInputBorder(),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class Hospital_page extends StatefulWidget {
+  @override
+  _HospitalPageState createState() => _HospitalPageState();
+}
+
+class _HospitalPageState extends State<Hospital_page> {
+  final TextEditingController _controller = TextEditingController();
+
+  @override
+  void dispose() {
+    _controller.dispose(); // Dispose the controller when the widget is removed
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Hospital Page'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            TextField(
+              controller: _controller,
+              decoration: InputDecoration(
+                labelText: 'Enter The Hospital name',
+                border: OutlineInputBorder(),
+              ),
+            ),
+            SizedBox(height: 20),
+            TextField(
+              controller: _controller,
+              decoration: InputDecoration(
+                labelText: 'Enter The hospital Phone Numbers',
+                border: OutlineInputBorder(),
+              ),
+            ),
+            SizedBox(height: 20),
+            TextField(
+               maxLines:10 ,
+              controller: _controller,
+              decoration: InputDecoration(
+                labelText: 'Enter The Names of the Doctors you have and their Specialization',
+                border: OutlineInputBorder(),
+              ),
+            ),
+            SizedBox(height: 20),
+            TextField(
+              controller: _controller,
+              decoration: InputDecoration(
+                labelText: 'Enter Your Ambulance Number.',
+                border: OutlineInputBorder(),
+              ),
+            ),
+            SizedBox(height: 20),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class Pharmacy_page extends StatefulWidget {
+  @override
+  _PharmacyPageState createState() => _PharmacyPageState();
+}
+
+class _PharmacyPageState extends State<Pharmacy_page> {
+  final TextEditingController _controller = TextEditingController();
+
+  @override
+  void dispose() {
+    _controller.dispose(); // Dispose the controller when the widget is removed
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Pharmacy Page'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            TextField(
+              controller: _controller,
+              decoration: InputDecoration(
+                labelText: 'Enter The Pharmacy Name',
+                border: OutlineInputBorder(),
+              ),
+            ),
+            SizedBox(height: 20),
+            TextField(
+              controller: _controller,
+              decoration: InputDecoration(
+                labelText: 'Enter The Pharmacy Phone Numbers',
+                border: OutlineInputBorder(),
+              ),
+            ),
+            SizedBox(height: 20),
+            TextField(
+              maxLines:20,
+              controller: _controller,
+              decoration: InputDecoration(
+                labelText: 'Enter The Names of the Medicines you have in Your Pharmacy',
+                border: OutlineInputBorder(),
+              ),
+            ),
+            SizedBox(height: 20),
+          ],
+        ),
+      ),
+    );
   }
 }
